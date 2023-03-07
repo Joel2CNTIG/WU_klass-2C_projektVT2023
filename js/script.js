22b2a791c7254a0b2a752b7ef9df04f7e45f369a
Joel <joel.fagersand@elev.ga.ntig.se>
2023-03-07 14:50:20 +0100

    ändringar på sida 1,3 och 4, lagt till popup reklam

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const burger = document.getElementById('burger_menu')
 const burgerList = document.getElementById('burger_list')
+const popupAd = document.getElementById('popup')
+const popupX = document.getElementById('x_popup')
+var counter = 0
 
 function toggleMenu() {
     burgerList.classList.toggle('burger_list_visible')
@@ -9,3 +12,24 @@ function toggleMenu() {
 burger.addEventListener('click', () => {
     toggleMenu()
 })
+
+
+
+function togglePopup() {
+    popupAd.classList.remove('popup_hidden')
+}
+
+
+window.addEventListener('scroll', () => {
+    counter = counter + 1
+    if (counter == 50) {togglePopup()}
+})
+
+function xTogglePopup() {
+    popupAd.classList.add('popup_hidden')
+}
+
+popupX.addEventListener('click', () => {
+    xTogglePopup()
+})
+
