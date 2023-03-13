df4e6992fbf3a6d76e6a153021696e3123d0d455
Joel <joel.fagersand@elev.ga.ntig.se>
2023-03-13 13:43:24 +0100

    större ändringar i sida 4

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ const burgerList = document.getElementById('burger_list')
 const popupAd = document.getElementById('popup')
 const popupX = document.getElementById('x_popup')
 var counter = 0
+var cycleLimit = 0
+const cycledElements = document.getElementsByClassName('cycled_reviews')
+const leftArrow = document.getElementById('arrow_1')
+const rightArrow = document.getElementById('arrow_2')
+cycleReview1 = document.getElementById('cycle_review1')
+cycleReview2 = document.getElementById('cycle_review2')
+cycleReview3 = document.getElementById('cycle_review3')
 
 function toggleMenu() {
     burgerList.classList.toggle('burger_list_visible')
@@ -32,4 +39,51 @@ function xTogglePopup() {
 popupX.addEventListener('click', () => {
     xTogglePopup()
 })
+
+function moveLeft() {
+
+    console.log("left")
+    console.log(cycleLimit)
+
+    if (cycleLimit == 1) {
+        cycleReview1.style.transform = "translateX(0px)"
+        cycleReview2.style.transform = "translateX(1200px)"
+        cycleReview3.style.transform = "translateX(2400px)"
+        cycleLimit = cycleLimit - 1
+        return;
+    }
+    if (cycleLimit == 2) {
+        cycleReview1.style.transform = "translateX(-1200px)"
+        cycleReview2.style.transform = "translateX(0px)"
+        cycleReview3.style.transform = "translateX(1200px)"
+        cycleLimit = cycleLimit - 1
+        return;
+    }
+}
+
+function moveRight() {
+
+    console.log("right")
+    console.log(cycleLimit)
+
+    if (cycleLimit == 0) {
+        cycleReview1.style.transform = "translateX(-1200px)"
+        cycleReview2.style.transform = "translateX(0px)"
+        cycleReview3.style.transform = "translateX(1200px)"
+        cycleLimit = cycleLimit + 1
+        return;
+    }
+    if (cycleLimit == 1) {
+        cycleReview1.style.transform = "translateX(-2400px)"
+        cycleReview2.style.transform = "translateX(-1200px)"
+        cycleReview3.style.transform = "translateX(0px)"
+        cycleLimit = cycleLimit + 1
+        return;
+    }
+}
+
+
+
+
+
 
